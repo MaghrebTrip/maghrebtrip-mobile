8db2bf071fb2e7d24be0709289d032af18c07c7e
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-04-11 19:18:52 +0000

    add places dependency

--- app/src/main/java/com/maghrebtrip/activities/MapsActivity.java
+++ app/src/main/java/com/maghrebtrip/activities/MapsActivity.java
@@ -3,6 +3,7 @@ package com.maghrebtrip.activities;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Window;
 
@@ -11,9 +12,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.maghrebtrip.R;
 import com.maghrebtrip.databinding.ActivityMapsBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -53,9 +63,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        final LatLng ATTRACTION1 = new LatLng(34.0258769,-6.8260299);
+        final LatLng ATTRACTION2 = new LatLng(34.0242107,-6.8231208);
+        final LatLng ATTRACTION3 = new LatLng(34.0222944,-6.8301424);
+
+        Marker markerAttraction1;
+        Marker markerAttraction2;
+        Marker markerAttraction3;
+
+        markerAttraction1 = mMap.addMarker(new MarkerOptions()
+                .position(ATTRACTION1)
+                .title("Attraction 1"));
+        markerAttraction1.setTag(0);
+
+        markerAttraction2 = mMap.addMarker(new MarkerOptions()
+                .position(ATTRACTION2)
+                .title("Attraction 2"));
+        markerAttraction2.setTag(0);
+
+        markerAttraction3 = mMap.addMarker(new MarkerOptions()
+                .position(ATTRACTION3)
+                .title("Attraction 3"));
+        markerAttraction3.setTag(0);
+
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .add(ATTRACTION1)
+                .add(ATTRACTION2)
+                .add(ATTRACTION3)
+                .color(Color.RED); // Set color of the polyline
+
+        mMap.addPolyline(polylineOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ATTRACTION1));
+
+        final String placeId = "ChIJ8QoUlJFrpw0RXGFGkplsNhM";
+
+        // Specify the fields to return.
+        final List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+        final FetchPlaceRequest request = FetchPlaceRequest.newInstance(placeId, placeFields);
+
+        PlacesClient placesClient = Places.createClient(getBaseContext());
+
     }
 }
